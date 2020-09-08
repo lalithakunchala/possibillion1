6ed9ccc6f89741310ab7ed7803ce2ca7a496df94
Lalitha Kunchala <lalithakunchala@gmail.com>
2020-09-08 22:57:06 +0530

    small changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors());
 var user = require('./routes/auth');
 app.use('/auth' ,user);
 
-var port = 8000;
+var port = process.env.PORT ||8000;
 app.listen(port, function(){
     console.log('app listening on port: '+port);
 });
